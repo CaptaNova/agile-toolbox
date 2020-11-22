fb30c540816510765b3e1e941ff3bd4deca4abee
Capta Nova <captanova@christoph-noelke.de>
2020-11-22 18:03:31 +0100

    feat(group-lottery): add global cities as group names
    Now the group names can be eiter fictive persons from Grimm fairytales
    or the largest cities of the world.

--- group-lottery/group.js
+++ group-lottery/group.js
@@ -2,6 +2,30 @@ const SHAKE_DURATION = 700;
 const MIN_GROUP_COUNT = 1;
 const MAX_GROUP_COUNT = 12;
 const GROUP_NAMES = {
+  city: [
+    // 3 largest urban areas from each continent
+    // see https://en.wikipedia.org/wiki/List_of_largest_cities
+    // Africa
+    "Kairo",
+    "Lagos",
+    "Kinshasa",
+    // Asia
+    "Tokio",
+    "Jakarta",
+    "Delhi",
+    // Europe
+    "Moskau",
+    "Paris",
+    "London",
+    // North America
+    "Mexico",
+    "New York",
+    "Los Angeles",
+    // South America
+    "São Paulo",
+    "Buenos Aires",
+    "Rio de Janeiro",
+  ],
   fairytale: [
     "Aschenputtel",
     "Dornröschen",
@@ -27,8 +51,15 @@ function shuffle(arr) {
   }
 }
 
+function getGroupNameCategory() {
+  const categories = Object.keys(GROUP_NAMES);
+  const index = Math.floor(Math.random() * categories.length);
+  return categories[index];
+}
+
 function getGroupNames() {
-  const groupNamesToUse = GROUP_NAMES.fairytale.map((name) => name);
+  const category = getGroupNameCategory();
+  const groupNamesToUse = GROUP_NAMES[category].map((name) => name);
   shuffle(groupNamesToUse);
   return groupNamesToUse;
 }
